1bd70f7293c783088a107f96b723d0ca48eaa704
= <=>
2022-05-02 12:38:04 -0500

    Se agrega Secuencia Categorias DAO para consumo de funcion en db

---
+++ src/main/java/com/baz/daos/categorias/SecuenciaCategoriasDAO.java
@@ -0,0 +1,11 @@
+package com.baz.daos.categorias;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class SecuenciaCategoriasDAO {
+
+}
