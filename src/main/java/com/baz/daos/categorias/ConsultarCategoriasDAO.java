08091d84df7ac2faefbed856871aa614daddec02
= <=>
2022-05-05 15:05:15 -0500

    Se anexa función para consulta de categorias por ID y DESCRIPCION

---
+++ src/main/java/com/baz/daos/categorias/ConsultarCategoriasDAO.java
@@ -0,0 +1,49 @@
+package com.baz.daos.categorias;
+
+import com.baz.models.CategoriasModel;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+@ApplicationScoped
+public class ConsultarCategoriasDAO {
+
+    @Inject
+    EntityManager entityManager;
+
+    /**
+     * <b>${consumeExistenciaFuncion}</b>
+     * @descripcion: Método DAO para consumo de función Existencia de Categorias en DB.
+     * @autor: Diego Vázquez Pérez
+     * @ultimaModificacion: 05/05/2022
+     */
+
+    @Transactional
+    public CategoriasModel consultarCategoriasFuncion(int idCategoria, String descripcionCategoria){
+
+        CategoriasModel categoriasModel = new CategoriasModel();
+
+        try{
+            StoredProcedureQuery storedProcedureQuery = entityManager
+                    .createStoredProcedureQuery("FNCATEGORIASEL")
+                    .registerStoredProcedureParameter("v_CATEGORIAID", BigDecimal.class, ParameterMode.IN)
+                    .registerStoredProcedureParameter("v_DESCCATEG", String.class, ParameterMode.IN)
+                    .setParameter("v_CATEGORIAID", idCategoria)
+                    .setParameter("v_DESCCATEG", descripcionCategoria);
+
+            storedProcedureQuery.execute();
+
+            categoriasModel = (CategoriasModel) storedProcedureQuery.getResultList();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return categoriasModel;
+    }
+}
