b6b27e4bf12f30775bb7dbe39dfcb058914498c3
= <=>
2022-05-10 16:54:57 -0500

    Se implementa método GET del servicio consultar campo

--- src/main/java/com/baz/campos/controller/CamposController.java
+++ src/main/java/com/baz/campos/controller/CamposController.java
@@ -1,13 +1,16 @@
 package com.baz.campos.controller;
 
+import com.baz.campos.models.CamposModel;
 import com.baz.campos.services.CamposService;
 import com.baz.categorias.dtos.GenericResponse;
 import com.baz.utils.Constantes;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,5 +37,31 @@ public class CamposController {
                 Constantes.HTTP_200,
                 Constantes.MENSAJE_EXITO,
                 camposService.listaOperacionesCampos());
+    }
+
+    /**
+     * <b>consultarCampo</b>
+     * @descripcion: Servicio GET para consultar campo
+     * @autor: Diego Vázquez Pérez
+     * @param idCampo Identificador del campo
+     * @param descripcionCampo Descripción del campo
+     * @ultimaModificacion: ${date}
+     */
+
+    @GET
+    @Path("/ConsultaCampo")
+    @Produces(MediaType.APPLICATION_JSON)
+    public GenericResponse<Iterable<CamposModel>> consultarCampo(
+            @Schema(example = "1", description = "Identificador del campo.")
+            @QueryParam("idCampo") Integer idCampo,
+            @Schema(example = "CLAVE", description = "Descripción del campo.")
+            @QueryParam("descripcionCategoria") String descripcionCampo){
+
+        Iterable<CamposModel> camposModels = camposService.consultarCampo(idCampo,
+                descripcionCampo);
+
+        return new GenericResponse<>(Constantes.HTTP_200,
+                Constantes.MENSAJE_EXITO,
+                camposModels);
     }
 }
