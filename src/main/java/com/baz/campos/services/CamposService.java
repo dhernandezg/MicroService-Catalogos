a4b4c7ac47205a5bf53fb0ce65cb62132fd76fa2
= <=>
2022-05-10 16:30:26 -0500

    Se implementa método para el servicio crear campo

--- src/main/java/com/baz/campos/services/CamposService.java
+++ src/main/java/com/baz/campos/services/CamposService.java
@@ -1,8 +1,13 @@
 package com.baz.campos.services;
 
+import com.baz.campos.daos.ActualizarCampoDao;
+import com.baz.campos.daos.ConsultarCampoDao;
+import com.baz.campos.daos.CrearCampoDao;
+import com.baz.campos.daos.EliminarCampoDao;
 import com.baz.categorias.dtos.GenericResponse;
 import com.baz.utils.Constantes;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,18 @@ import java.util.List;
 
 @Singleton
 public class CamposService {
+
+    @Inject
+    CrearCampoDao crearCampoDao;
+
+    @Inject
+    ConsultarCampoDao consultarCampoDao;
+
+    @Inject
+    ActualizarCampoDao actualizarCampoDao;
+
+    @Inject
+    EliminarCampoDao eliminarCampoDao;
 
     /**
      * <b>listaOperacionesCampos</b>
@@ -37,5 +54,22 @@ public class CamposService {
         lista.add(Constantes.DELETE + Constantes.CAMPOS);
 
         return lista;
+    }
+
+    /**
+     * <b>crearCampo</b>
+     * @descripcion: Método para invocar crear campo.
+     * @autor: Diego Vázquez Pérez
+     * @param descripcionCampo Descripcion del campo
+     * @param usuarioNombre Nombre del usuario
+     * @ultimaModificacion: 10/05/2022
+     */
+
+    public boolean crearCampo(String descripcionCampo,
+                              String usuarioNombre){
+
+        return crearCampoDao.crearCampo(descripcionCampo,
+                usuarioNombre);
+
     }
 }
