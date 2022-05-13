3a84ffb9c36439989d7724b338a51c2002f68f77
= <=>
2022-05-12 19:55:17 -0500

    Se implementa método POST para crear origen

--- src/main/java/com/baz/origen/controller/OrigenController.java
+++ src/main/java/com/baz/origen/controller/OrigenController.java
@@ -3,11 +3,10 @@ package com.baz.origen.controller;
 import com.baz.categorias.dtos.GenericResponse;
 import com.baz.origen.services.OrigenService;
 import com.baz.utils.Constantes;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 
@@ -33,5 +32,38 @@ public class OrigenController {
                 Constantes.HTTP_200,
                 Constantes.MENSAJE_EXITO,
                 origenService.listaOperacionesOrigen());
+    }
+
+    /**
+     * <b>crearOrigen</b>
+     * @descripcion: Método POST para crear origen
+     * @autor: Diego Vázquez Pérez
+     * @param descripcionOrigen Descripción del origen
+     * @param claveOrigen CLva del origen
+     * @param usuarioNombre Nombre del usuario que registra
+     * @ultimaModificacion: 12/05/2022
+     */
+
+    @POST
+    @Path("/CrearOrigen")
+    @Produces(MediaType.APPLICATION_JSON)
+    public GenericResponse<Boolean> crearOrigen(
+            @Schema(example = "Inteligencia de Datos e Innovación", description = "Nombre del área de origen")
+            @QueryParam("descripcionOrigen") String descripcionOrigen,
+
+            @Schema(example = "IDI", description = "Clave del área de origen")
+            @QueryParam("claveOrigen") String claveOrigen,
+
+            @Schema(example = "Nombre Apellido1 Apellido2", description = "Nombre del usuario que registra")
+            @QueryParam("usuarioNombre") String usuarioNombre){
+
+        boolean response = origenService.crearOrigen(descripcionOrigen,
+                claveOrigen,
+                usuarioNombre);
+
+        return new GenericResponse<>(
+                Constantes.HTTP_200,
+                Constantes.MENSAJE_EXITO,
+                response);
     }
 }
