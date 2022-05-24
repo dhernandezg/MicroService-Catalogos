42803c0e5340713d97f84a2a73ec24ee5bc7b062
= <=>
2022-05-24 18:41:06 -0500

    Se implementa servicio de consulta de elementos bse mediante parametros especificos o todos los elementos mediante parametros vacios

--- src/main/java/com/baz/elementosbase/services/ElementosBaseService.java
+++ src/main/java/com/baz/elementosbase/services/ElementosBaseService.java
@@ -6,10 +6,13 @@ import com.baz.elementosbase.daos.CrearElementoBaseDao;
 import com.baz.elementosbase.daos.EliminarElementoBaseDao;
 import com.baz.elementosbase.models.ActualizarElementoBaseModel;
 import com.baz.elementosbase.models.CrearElementoBaseModel;
+import com.baz.elementosbase.models.ElementosBaseModel;
 import com.baz.elementosbase.models.EliminarElementoBaseModel;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 /**
  * <b>ElementosBaseService</b>
@@ -41,7 +44,7 @@ public class ElementosBaseService {
      * <b>${nombreClase}</b>
      * @descripcion: Método para crear un elemento base
      * @autor: Diego Vázquez Pérez
-     * @param elemento Datos requeridos para crear un elemento base
+     * @param elemento Modelo de datos requeridos para crear un elemento base
      * @ultimaModificacion: 24/05/2022
      */
 
@@ -50,5 +53,27 @@ public class ElementosBaseService {
     ){
 
         return crearElementoBaseDao.crearElementoBase(elemento);
+    }
+
+    /**
+     * <b>consultarElementosBase</b>
+     * @descripcion: Método para consultar un elemento mediante parámetros específicos
+     * o todos los elementos mediante parámetros vacíos.
+     * @autor: Diego Vázquez Pérez
+     * @param idCategoria Identificador de la categoría a la que pertenece el cátalogo
+     * @param idCatalogo Identificador del catálogo al que pertenece el elemento base
+     * @param idElementoBase Identificador del elemento base a consultar
+     * @ultimaModificacion: 24/05/2022
+     */
+
+    public List<ElementosBaseModel> consultarElementosBase(
+            Integer idCategoria,
+            Integer idCatalogo,
+            Integer idElementoBase){
+
+        return consultarElementosBaseDao.consultarElementosBase(
+                idCategoria,
+                idCatalogo,
+                idElementoBase);
     }
 }
