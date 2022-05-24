253f5d5707df26303c56ffbb41a82507a13d3442
= <=>
2022-05-24 11:27:00 -0500

    Se implementa modelo de la entidad Estatus

---
+++ src/main/java/com/baz/estatus/models/EstatusModel.java
@@ -0,0 +1,40 @@
+package com.baz.estatus.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EstatusModel {
+
+    @Schema(description = "Identificador del estatus.")
+    @Column(name = "statusid")
+    @Id
+    private Integer idEstatus;
+
+    @Schema(description = "Descripción del estatus.")
+    @Column(name = "descstatus")
+    private String descripcionEstatus;
+
+    @Schema(description = "Fecha de creación del estatus.")
+    @Column(name = "fechacrea")
+    private String fechaCreacion;
+
+    @Schema(description = "Fecha de modificación del estatus.")
+    @Column(name = "fechamod")
+    private String fechaModificacion;
+
+    @Schema(description = "Nombre del usuario que registró el estatus.")
+    @Column(name = "usuariocrea")
+    private String usuarioCreacion;
+
+    @Schema(description = "Nombre del usuario que modificó el estatus.")
+    @Column(name = "usuariomod")
+    private String usuarioModificacion;
+}
